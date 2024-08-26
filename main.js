e126ba3fbc3798fc89112f011f825a9c7a442ec3
Carlos Vieira <112698218+carloeira@users.noreply.github.com>
2024-08-26 20:57:30 -0300

    aula inicial

---
+++ main.js
@@ -0,0 +1,35 @@
+// // Estrutura Sequencial
+
+// let primeiroNum = parseInt(prompt("Digite o primeiro número: "));
+// let segundoNum = parseInt(prompt("Digite o segundo número: "));
+
+// let soma = primeiroNum + segundoNum;
+
+// console.log("A soma dos números é: " + soma);
+
+// // Estrutura de Decisão
+
+// let num = parseInt(prompt("Digite um número: "));
+// if (num > 0) {
+//     console.log("O número é positivo.");
+// } else {
+//     console.log("O número é negativo ou igual a zero");
+// }
+
+// // Estrutura de Repetição
+
+// let contador = 1;
+// while (contador <= 10) {
+//     console.log(contador);
+//     contador++;
+// }
+
+// // Estrutura de Controle
+
+// for (let i = 0; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i, "é par");
+//     } else {
+//         console.log(i, "é ímpar");
+//     }
+// }
